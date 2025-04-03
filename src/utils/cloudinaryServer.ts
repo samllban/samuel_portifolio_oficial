f002837ca33308bf77af0d60ef7e206b88b461c4
samllban <137834390+samllban@users.noreply.github.com>
2025-04-03 12:25:48 -0300

    correção

--- src/utils/cloudinaryServer.ts
+++
@@ -1,9 +0,0 @@
-import { getTransformedImage } from '@/lib/cloudinary/transformations';
-
-export async function getOptimizedImageUrl(
-    imageName: string,
-    options?: GetTransformedImageParams
-) {
-    'use server';
-    return getTransformedImage({ imageName, ...options }).toURL();
-}
