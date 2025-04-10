b31645ef70cbe98dd8e22872574642c604f9be3c
samllban <137834390+samllban@users.noreply.github.com>
2025-04-10 11:54:10 -0300

    resolução acessibilidade

--- src/components/sections/AppleCardsCarousel/AppleCardsCarousel.tsx
+++ src/components/sections/AppleCardsCarousel/AppleCardsCarousel.tsx
@@ -10,7 +10,7 @@ export function AppleCardsCarouselDemo() {
 
     return (
         <div className="w-full h-full py-20">
-            <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+            <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-white font-sans">
                 Portfólio
             </h2>
             <Carousel items={cards} />
@@ -27,8 +27,8 @@ const DummyContent = () => {
                         key={"dummy-content" + index}
                         className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
                     >
-                        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
-                            <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                        <p className="text-neutral-300 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+                            <span className="font-bold text-neutral-100">
                                 The first rule of Apple club is that you boast about Apple club.
                             </span>{" "}
                             Keep a journal, quickly jot down a grocery list, and take amazing
